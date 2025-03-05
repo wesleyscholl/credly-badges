f9a30262167f2658c406e4ad15f3d46616a75426
Wesley Scholl <128409641+wesleyscholl@users.noreply.github.com>
2025-03-05 16:35:56 -0500

    Update credly.py

--- services/credly.py
+++ services/credly.py
@@ -36,10 +36,12 @@ class Credly:
         img = soupBadge.findAll(
             "img", {"class": "cr-standard-grid-item-content__image"}
         )[0]
+        issuer = soupBadge.find("div", {"class": "cr-standard-grid-item-content__issuer"}).text.strip()
         return {
             "title": str(htmlBadge["title"]).replace('"', '\\"'),
             "href": self.BASE_URL + htmlBadge["href"],
             "img": img["src"].replace("110x110", f"{BADGE_SIZE}x{BADGE_SIZE}"),
+            "issuer": issuer,
         }
 
     def return_badges_html(self):
@@ -47,15 +49,32 @@ class Credly:
         soup = BeautifulSoup(data, "lxml")
         return soup.findAll("a", {"class": "cr-public-earned-badge-grid-item"})
 
-    def generate_md_format(self, badges):
+    def generate_table_format(self, badges):
         if not badges:
             return None
-        return "\n".join(
-            map(
-                lambda it: f"[![{it['title']}]({it['img']})]({it['href']} \"{it['title']}\")",
-                badges,
-            )
-        )
+        issuers = {}
+        for badge in badges:
+            issuer = badge['issuer']
+            if issuer not in issuers:
+                issuers[issuer] = []
+            issuers[issuer].append(badge)
+
+        tables = []
+        for issuer, issuer_badges in issuers.items():
+            rows = []
+            for i in range(0, len(issuer_badges), 5):
+                row = issuer_badges[i:i+5]
+                rows.append("<tr>")
+                for badge in row:
+                    rows.append(f'<td width="20%"><a href="{badge["href"]}"><img src="{badge["img"]}" /></a></td>')
+                rows.append("</tr><tr>")
+                for badge in row:
+                    rows.append(f'<td align="center" width="20%"><a href="{badge["href"]}">{badge["title"]} - {issuer}</a></td>')
+                rows.append("</tr>")
+            table = f'<table width="100%">{" ".join(rows)}</table>'
+            tables.append(table)
+
+        return "\n".join(tables)
 
     def get_markdown(self):
         badges_html = (
@@ -63,6 +82,6 @@ class Credly:
             if NUMBER_LAST_BADGES > 0
             else self.return_badges_html()
         )
-        return self.generate_md_format(
+        return self.generate_table_format(
             [self.convert_to_dict(badge) for badge in badges_html]
         )
