b2136eddfd7ba8834cb759a740f34cadf8071586
Wesley Scholl <128409641+wesleyscholl@users.noreply.github.com>
2025-03-06 14:59:07 -0500

    Fix: Remove debug print statement

--- services/credly.py
+++ services/credly.py
@@ -31,14 +31,14 @@ class Credly:
             url = f"{self.BASE_URL}/users/{self.USER}/badges?page={page}&sort={self.sort_by()}"
             response = requests.get(url)
             data = response.text
-            print(data)
 
             soup = BeautifulSoup(data, "lxml")
             badges = soup.findAll("a", {"class": "cr-public-earned-badge-grid-item"})
             if not badges:
                 break
 
-            all_data += data
+            relevant_data = ''.join(str(badge) for badge in badges)
+            all_data += relevant_data
 
             next_page = soup.find("a", {"rel": "next"})
             if not next_page:
