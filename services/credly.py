094bf775b1451a9bdfc5617289c56a81313e1dc9
Wesley Scholl <128409641+wesleyscholl@users.noreply.github.com>
2025-05-08 10:12:12 -0400

    Fix: Add missing headers to Credly request

--- services/credly.py
+++ services/credly.py
@@ -28,7 +28,11 @@ class Credly:
         all_data = ""
         page = 1
         headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Connection": "keep-alive",
+        "Upgrade-Insecure-Requests": "1"
     }
 
         while True:
